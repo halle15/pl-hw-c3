ca68b6361cbfdae176423b5205095f0d08e3c207
ehall <halle15@email.franklin.edu>
2022-11-26 03:33:13 -0500

    added header, created format

--- c3.c
+++ c3.c
@@ -1,3 +1,47 @@
-
+#include <stdio.h>
+#include <ctype.h>
+#include <string.h>
+#include "c3.h"
 // init commit
 
+int whichChar;
+struct Token thisToken;
+
+
+
+
+void match(TokenType tkType){
+
+
+
+}
+
+void parse(){
+
+    //parse will keep track of which character the iterator is on with whichchar.
+    //
+
+}
+
+struct Token getToken(void){
+    struct Token tk;
+
+    // this function is meant to read the first character and then return the relevant token.
+
+    return tk;
+}
+
+int main(){
+
+    char userString[100];
+
+    scanf("%s", userString);
+
+    char c = ' ';
+
+    int i = isspace(c);
+    
+    printf("True: %i ", i);
+
+    return 1;
+}
---
+++ c3.h
@@ -0,0 +1,18 @@
+
+typedef enum {PLUS, MINUS, DIVIDE, MULT, REMAINDER, POWER, LPAREN, RPAREN, NUMBER, ERROR, EOL} TokenType;
+
+struct Token {
+    TokenType type;
+    int value;
+};
+
+int expr(void);
+int term(void);
+int power(void);
+int factor(void);
+ 
+int factor1(void);
+void error(void);
+void parse(void);
+void match(TokenType tkType);
+struct Token getToken(void);
